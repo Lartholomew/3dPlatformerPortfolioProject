0556ce7c8c64adebfd9b639701c15046acd588f0
Will <48697015+Lartholomew@users.noreply.github.com>
2023-08-11 13:34:03 -0700

    updating end state UI prefab

---
+++ 3dGrappleHookWallRunner/Assets/UI/EndStateUIData.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+// Holds all the images and such that the end state will use and take from this scriptable object
+[CreateAssetMenu(menuName = "Create EndStateUIData", fileName = "NewEndStateUIData")]
+public class EndStateUIData : ScriptableObject
+{
+    
+}
